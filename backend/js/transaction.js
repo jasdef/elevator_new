7d5d9b8af555d636d2e2f435054a00865eceafdf
chapman <chapmanlin51@gmail.com>
2019-03-09 15:43:04 +0800

    新增買賣單 基本功能已做完

--- backend/js/transaction.js
+++ backend/js/transaction.js
@@ -50,6 +50,54 @@ router.post('/GetTransactionList', function (req, res) {
 
 });
 
+router.post('/AddTransaction', function(req, res) {//4
+    common.CreateHtml("Transaction_Transfer", req, res, function (err) {
+    common.BackendConnection(res, function(err, connection) {
+            var requestData = JSON.parse(req.body.requestData);
+            console.log(req.body);
+            console.log(requestData);
+            var addTransactionSQL = "insert into transaction_form (`title`, `left_price`, `total_price`, `start_date`, `is_return`, `is_duty`, `is_receipt`, `elevator_num`, `note`, `customer_id`, `items`) VALUES (?,?,?,?,?,?,?,?,?,?,?);";
+
+            var historyData = [requestData.MemberId, requestData.OldPhoneNum, requestData.NewPhoneNum, req.session['account'], requestData.Note];
+            var itemsJson = JSON.stringify(requestData.items);
+            var transactionData = 
+            [
+                requestData.title, 
+                requestData.leftPrice,
+                requestData.totalPrice,
+                requestData.startDate,
+                requestData.isReturn,
+                requestData.isDuty,
+                requestData.isReceipt,
+                requestData.elevatorNum,
+                requestData.note,
+                requestData.customerId,
+                itemsJson
+            ];
+  
+            addTransactionSQL = connection.format(addTransactionSQL, transactionData);
+
+            var sql = addTransactionSQL;
+
+            common.log(req.session['account'], sql);
+            connection.query(sql, function (error, result, fields) {
+                if (error) {
+                    common.log(req.session['account'], error);
+                    connection.release();                    
+                    res.send({ code: -1, msg: "新增失敗", err: error }).end();
+
+                }
+                else {
+                    connection.release();                    
+                    res.send({ code: 0, msg: "新增成功!" }).end();
+                }
+            });
+
+    });
+    });
+});
+
+
 router.post('/CheckMemberId', function(req, res) {
     common.CreateHtml("SimpleDeposit_Transfer", req, res, function () {
         common.log(req.session['account'], "Call CheckMember");

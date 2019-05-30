7330e04a4c9fd28417fb0f6d5e558abe086c865a
chapman <chapmanlin51@gmail.com>
2019-05-30 17:21:38 +0800

    完成保養單提醒流程

--- backend/js/service.js
+++ backend/js/service.js
@@ -10,25 +10,26 @@ router.get('/Service', function(req, res) {
 router.get('/ServiceAdd', function(req, res) {
     common.log(req.session['account'], 'call Service add');
     common.CreateHtml("ServiceAdd", req, res);
-
 });
 
 router.get('/ServiceEdit', function(req, res) {
     common.log(req.session['account'], 'call Service edit');
     common.CreateHtml("ServiceEdit", req, res);
-
 });
 
 router.get('/ServiceView', function(req, res) {
     common.log(req.session['account'], 'call Service view');
     common.CreateHtml("ServiceView", req, res);
-
 });
 
 router.get('/ServiceStaffView', function(req, res) {
     common.log(req.session['account'], 'call Service staff view');
     common.CreateHtml("ServiceStaffView", req, res);
+});
 
+router.get('/ServiceRemind', function(req, res) {
+    common.log(req.session['account'], 'call Service remind');
+    common.CreateHtml("ServiceRemind", req, res);
 });
 
 router.post('/GetServiceList', function (req, res) {
@@ -119,8 +120,8 @@ router.post('/EditService', function(req, res) {
                 requestData.hasLicense,
                 requestData.licenseDate,
                 itemsJson,
-                requestData.id,
-                requestData.doTimes
+                requestData.doTimes,
+                requestData.id
             ];
   
             editServiceSQL = connection.format(editServiceSQL, serviceData);
@@ -143,6 +144,112 @@ router.post('/EditService', function(req, res) {
 
     });
     });
+});
+
+router.post('/CheckServiceRemind', function(req, res) {
+    common.CreateHtml("Service_Transfer", req, res, function (err) {
+    common.BackendConnection(res, function(err, connection) {
+   
+            var sql = "select * from service_form where is_remind=0 and is_delete=0;";
+
+            common.log(req.session['account'], sql);
+            connection.query(sql, function (error, result, fields) {
+                if (error) {
+                    common.log(req.session['account'], error);
+                    connection.release();                    
+                    res.send({ code: -1, err: error }).end();
+
+                }
+                else if (result.length > 0){
+                    var data = result;
+                    var updateRemindStatus = "";
+                    for (var i = 0; i < data.length; i++) {
+                        var tempTime = new Date(data[i].start_date);
+                        if (tempTime < new Date()) {                 
+                            var startMonth = tempTime.toLocaleString().split("-")[1];                           
+                            var dispatchMonth = data[i].dispatch_month;
+
+                            if (dispatchMonth == 0) {//for第一次
+                                dispatchMonth = startMonth;
+                              //  var totalTimes = data[i].mechanical_warranty * 12 / data[i].service_month * data[i].do_times; 
+                                updateRemindStatus += "update service_form set is_remind=1, dispatch_month="+dispatchMonth+" where id="+data[i].id+" and is_delete=0;";
+                            }
+                            else {
+                                updateRemindStatus += `update service_form set is_remind=1 where id=`+data[i].id+" and is_delete=0;";
+                            }                           
+                        }
+                    }
+                    
+                    if (updateRemindStatus != "") {
+                        common.log("System", updateRemindStatus);
+                        connection.query(updateRemindStatus, function (error, result, fields) {
+                            if (error) {
+                                common.log(req.session['account'], error);
+                                res.send({error : error});
+                            }
+                            
+                            res.send({ msg: "done" });
+                            connection.release();
+                            res.end();
+    
+                        });
+                    }
+                    else {
+
+                        common.log("System", "no need to remind warranty");
+                        res.send({ msg: "done" });
+                        connection.release();
+                        res.end();
+                    }
+                }
+                else {
+                    res.send({ msg: "done" });
+                    connection.release();
+                    res.end();
+                }
+            });
+        });
+    });
+});
+
+router.post('/GetServiceRemindList', function (req, res) {
+    common.CreateHtml("Service_Transfer", req, res, function (err) {
+        common.BackendConnection(res, function (err, connection) {
+            if (err) {
+                common.log(res.session['account'], err);
+                throw err;
+            }
+            
+            var nowMonth = new Date().toLocaleString().split("-")[1];
+
+            var dataSelect = "select a.id, a.mechanical_warranty, a.do_times, a.service_month, a.start_date, b.title, a.service_times from service_form as a inner join warranty_form as b on a.warranty_id=b.id where a.is_delete=0 and a.is_remind=1 and a.is_dispatch=0 and a.dispatch_month="+nowMonth+";";
+            var countSelect = "select COUNT(*) as count from service_form as a inner join warranty_form as b on a.warranty_id=b.id where a.is_delete=0 and a.is_remind=1 and a.is_dispatch=0 and a.dispatch_month="+nowMonth+";";   
+            var sql = countSelect + dataSelect;
+
+            common.log(req.session['account'], sql);
+
+            connection.query(sql, function (error, result, fields) {
+                if (error) {
+                    common.log(req.session['account'], error);
+                    res.send({error : error});
+                }
+                else {
+                    var totallength = result[0][0].count;
+
+                    for (var i = 0; i < totallength; i++) {               
+                        var needDoTimes = result[1][i].mechanical_warranty * 12 / result[1][i].service_month * result[1][i].do_times; 
+                        result[1][i].total_times = needDoTimes;                    
+                    }
+
+                    res.send({ recordsTotal: totallength, recordsFiltered: totallength, data: result[1] });
+                }
+                connection.release();
+                res.end();
+            });
+
+        });        
+    });
+
 });
 
 router.post('/AddServiceByWarrantyID', function(req, res) {
